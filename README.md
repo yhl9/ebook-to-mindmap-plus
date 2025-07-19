7822b1cb859261ce92283122087ddc3dead93c5f
DJJo <ssshooterx@gmail.com>
2025-07-19 16:40:27 +0800

    Initialize shadcn

---
+++ README.md
@@ -0,0 +1,7 @@
+# Template for vibe coding
+
+- latest react
+- latest tailwind
+- use vite for high build speed
+
+you can also try branch `shadcn`
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold">
+      react-tailwind-template-for-vibe-coding
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
